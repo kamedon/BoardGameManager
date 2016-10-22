907625544c384e1bbe2668d324e77d25256d2705
kamedon <hkamei06@gmail.com>
2016-10-22 14:31:45 +0900

    entity作成

---
+++ app/src/main/java/com/kamedon/boardgamemanager/domain/entity/BoradGame.kt
@@ -0,0 +1,12 @@
+package com.kamedon.boardgamemanager.domain.entity
+
+/**
+ * Created by kamei.hidetoshi on 2016/10/22.
+ */
+
+class BoradGame {
+    var key: String = ""
+    var barcode: String = ""
+    var name: String = ""
+    var price: String = ""
+}
